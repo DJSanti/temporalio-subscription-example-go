5ee1285a15190c720b3b7b9f8b93d1c350bc2207
Samantha Santiago <samantha@temporal.io>
2023-02-01 11:53:18 -0600

    rewriting

--- workflow.go
+++ workflow.go
@@ -1,1 +1,56 @@
 package app
+
+import (
+	"go.temporal.io/sdk/workflow"
+)
+
+type ComposeEmail struct {
+	Email string
+	Message string
+	count int
+}
+
+type BillingInfo struct {
+	Email string
+	MaxBillingPeriods int
+	EmailsSent int
+	isSubscribed bool
+	isCanceled bool
+
+}
+
+// FreeTrialWorkflow definition
+func SubscriptionWorkflow(ctx workflow.Context) {
+	// set up logger
+	logger := workflow.GetLogger(ctx)
+
+	// declare struct here
+	var PendingEmail ComposeEmail
+
+	// set up query handler
+	err := workflow.SetQueryHandler(ctx, "getBillingInfo", "") 
+	if err != nil {
+		logger.Info("SetQueryHandler failed.", "Error", err)
+	}
+
+	// set up signal channel(s)
+
+	// workflow logic here
+
+	// signal handler for welcome email
+
+	// signal handler for cancellation
+
+	// signal handler for expired subscription
+
+	// check billing period for expiration
+
+}
+
+// set free trial state
+
+// set subscription state
+
+
+
+	
